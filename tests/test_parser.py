eff218baf07f4a8ee3b76937c1382d4e892a206a
fsoza <fsozale@gmail.com>
2025-11-25 12:15:59 -0300

    Fix tests parser file encoding

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -4,14 +4,14 @@ from pygwt.parser import GwtParser
 
 
 def test_parse_examples(gwt_file, gwt_models):
-    text = Path(gwt_file).read_text()
+    text = Path(gwt_file).read_text(encoding="utf-8")
     parser = GwtParser(text, gwt_models)
     result = parser.parse()
     assert isinstance(result, list)
 
 
 def test_target_file_not_empty(target_file, gwt_models):
-    text = Path(target_file).read_text()
+    text = Path(target_file).read_text(encoding="utf-8")
     parser = GwtParser(text, gwt_models)
     result = parser.parse()
     assert isinstance(result, list)
